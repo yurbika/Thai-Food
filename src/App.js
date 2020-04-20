1983a83f99c7979faa0de006462425b6f629c098
Berkay Yurdagül <yurdaguel.b@gmail.com>
2020-04-20 11:33:15 +0200

    adding react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { Switch, Route } from "react-router-dom";
 
 import "./App.css";
 
 import Home from "./pages/home/home.component";
+import Navbar from "./components/navbar/navbar.component";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Navbar />
+      <Suspense fallback={"loading..."}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Suspense>
     </div>
   );
 }
