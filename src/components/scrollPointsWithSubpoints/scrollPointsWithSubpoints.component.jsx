bd15ee1661390333c4e9c1473ef940a29d06a024
Berkay Yurdagül <yurdaguel.b@gmail.com>
2020-04-24 18:16:42 +0200

    finding a way of subpoints to set counter the right number

--- src/components/scrollPointsWithSubpoints/scrollPointsWithSubpoints.component.jsx
+++ src/components/scrollPointsWithSubpoints/scrollPointsWithSubpoints.component.jsx
@@ -7,12 +7,26 @@ import ID_GENERATOR from "../../utils/uniqueKey";
 import { Point, Container, Title } from "./scrollPointsWithSubpoints.styles";
 
 const ScrollPointsWithSubpoints = ({ lengthArr, namesArr }) => {
+  var counter = 0;
+  const giveRightPos = (arr, index, curPos = 0, posNum = 0) => {
+    console.log(curPos, posNum);
+    if (index >= arr.length) return;
+    if (index === 0) return posNum;
+    if (lengthArr[curPos] === 0) ++curPos;
+    posNum++;
+    lengthArr[curPos] = --lengthArr[curPos];
+    giveRightPos(lengthArr, index - 1, curPos, posNum);
+  };
+
+  //  console.log(giveRightPos(lengthArr, 1));
+
   return (
     <Container>
       {lengthArr.map((length, index) => {
         if (length === 1)
           return (
             <Title active key={ID_GENERATOR("title-points-")}>
+              {console.log((counter = ++counter))}
               {namesArr[index].toUpperCase()}
             </Title>
           );
@@ -20,11 +34,13 @@ const ScrollPointsWithSubpoints = ({ lengthArr, namesArr }) => {
           return (
             <div key={ID_GENERATOR("container-of-points-")}>
               <Title active key={ID_GENERATOR("title-of-points-")}>
+                {console.log((counter = ++counter))}
+
                 {namesArr[index].toUpperCase()}
               </Title>
               <div>
                 {Array.from({ length: length }, (_, i) => {
-                  console.log(index + length + i);
+                  console.log((counter += i));
                   return <Point active key={ID_GENERATOR("points-")} />;
                 })}
               </div>
