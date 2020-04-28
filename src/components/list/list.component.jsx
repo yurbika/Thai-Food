d963b149eb315ae35eceebb0ef3af5ff5db87264
Berkay Yurdagül <yurdaguel.b@gmail.com>
2020-04-28 10:17:06 +0200

    adjusting styles and component to visibility hidden, to hidde content

--- src/components/list/list.component.jsx
+++ src/components/list/list.component.jsx
@@ -1,20 +1,25 @@
 import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+//redux
+import { selectMenuSubCounter } from "../../redux/menu/menu.selectors";
 
 //styles
 import { Container, ScrollContainer } from "./list.styles";
 
-const List = ({ chunkedArray }) => {
+const List = ({ chunkedArray, subcounter }) => {
   return (
-    <ScrollContainer>
-      {chunkedArray.map((item) => (
-        <Container>
+    <ScrollContainer marginValue={100 * subcounter}>
+      {chunkedArray.map((item, index) => (
+        <Container active={subcounter === index}>
           {item.map((food) =>
             Object.keys(food).map((key) => (
               <div>
-                <span>{key}.</span>
-                <span>{food[key]["name"]}</span>
-                <span>{food[key]["additionalInfo"]}</span>
-                <span>
+                <span tabIndex={0}>{key}.</span>
+                <span tabIndex={0}>{food[key]["name"]}</span>
+                <span tabIndex={0}>{food[key]["additionalInfo"]}</span>
+                <span tabIndex={0}>
                   {Number(food[key]["price"]).toLocaleString("es-Es", {
                     minimumFractionDigits: 2,
                   }) + " "}
@@ -29,4 +34,8 @@ const List = ({ chunkedArray }) => {
   );
 };
 
-export default List;
+const mapStateToProps = createStructuredSelector({
+  subcounter: selectMenuSubCounter,
+});
+
+export default connect(mapStateToProps)(List);
--- src/components/list/list.styles.jsx
+++ src/components/list/list.styles.jsx
@@ -1,12 +1,34 @@
 import styled, { css } from "styled-components";
+import theme from "../../utils/theme";
+
+const getAdditionalStylesContainer = (props) => {
+  if (props.active)
+    return css`
+      visibility: visible;
+    `;
+};
 
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-content: space-between;
   height: 100vh;
+  visibility: hidden;
+
+  ${getAdditionalStylesContainer}
 `;
+
+const getAdditionalStyles = (props) => {
+  if (props.marginValue)
+    return css`
+      margin-top: -${props.marginValue}vh;
+    `;
+};
 
 export const ScrollContainer = styled.div`
   overflow: hidden;
+  transition: all ${theme.animationTimes["500"] + "ms"} ease-in-out;
+  opacity: 1;
+
+  ${getAdditionalStyles}
 `;
