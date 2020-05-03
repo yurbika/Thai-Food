960b9efe4b0450b0e0a7832822f76f693b08fa38
Berkay Yurdagül <yurdaguel.b@gmail.com>
2020-05-03 22:59:37 +0200

    adding img to contact working on styling

--- src/pages/contact/contact.component.jsx
+++ src/pages/contact/contact.component.jsx
@@ -3,6 +3,9 @@ import React from "react";
 //components
 import Background from "../../components/background/background.component";
 
+//assets
+import Food from "../../assets/contact.jpg";
+
 //styles
 import {
   Container,
@@ -11,63 +14,124 @@ import {
   BHContainer,
   Row,
   RowHeader,
+  ImgContainer,
+  Wrapper,
 } from "./contact.styles";
 
 const Contact = () => (
   <Container>
     <Background className={"background"} />
-    <Content>
-      <Info>
-        <span>Thai Food</span>
-        <span>Cranachstraße 51</span>
-        <span>31691 Niedersachsen</span>
-        <span>+49 157 6874 3357</span>
-      </Info>
 
-      <BHContainer role="table" aria-label="Business hours">
-        <Row role="row">
-          <RowHeader role="rowheader" aria-label="Monday">
-            MON
-          </RowHeader>
-          <span role="cell">9:00 A.M. - 10:00 P.M.</span>
-        </Row>
-        <Row role="row">
-          <RowHeader role="rowheader" aria-label="Tuesday">
-            TUE
-          </RowHeader>
-          <span role="cell">9:00 A.M. - 10:00 P.M.</span>
-        </Row>
-        <Row role="row">
-          <RowHeader role="rowheader" aria-label="Wednesday">
-            WED
-          </RowHeader>
-          <span role="cell">9:00 A.M. - 10:00 P.M.</span>
-        </Row>
-        <Row role="row">
-          <RowHeader role="rowheader" aria-label="Thursday">
-            THU
-          </RowHeader>
-          <span role="cell">9:00 A.M. - 10:00 P.M.</span>
-        </Row>
-        <Row role="row">
-          <RowHeader role="rowheader" aria-label="Friday">
-            FRI
-          </RowHeader>
-          <span role="cell">9:00 A.M. - 10:00 P.M.</span>
-        </Row>
-        <Row role="row">
-          <RowHeader role="rowheader" aria-label="Saturday">
-            SAT
-          </RowHeader>
-          <span role="cell">12:00 A.M. - 10:00 P.M.</span>
-        </Row>
-        <Row role="row">
-          <RowHeader role="rowheader" aria-label="Sunday">
-            SUN
-          </RowHeader>
-          <span role="cell">12:00 A.M. - 10:00 P.M.</span>
-        </Row>
-      </BHContainer>
+    <Content>
+      <ImgContainer>
+        <img src={Food} alt="" />
+      </ImgContainer>
+      <Wrapper>
+        <Info>
+          <span tabIndex={0} aria-label="We are Thai Food">
+            Thai Food
+          </span>
+          <span tabIndex={0} aria-label="adress, Cranachstraße 51">
+            Cranachstraße 51
+          </span>
+          <span
+            tabIndex={0}
+            aria-label="postcode, 31691, federal state, niedersachsen"
+          >
+            31691 Niedersachsen
+          </span>
+          <span tabIndex={0} aria-label="Phone number, +49 157 6874 3357">
+            +49 157 6874 3357
+          </span>
+        </Info>
+
+        <BHContainer role="table" tabIndex={0} aria-label="Business hours">
+          <Row role="row">
+            <RowHeader role="rowheader" tabIndex={0} aria-label="Monday">
+              MON
+            </RowHeader>
+            <span
+              role="cell"
+              tabIndex={0}
+              aria-label="from 9:00 A.M. till 10:00 P.M."
+            >
+              9:00 A.M. - 10:00 P.M.
+            </span>
+          </Row>
+          <Row role="row">
+            <RowHeader role="rowheader" tabIndex={0} aria-label="Tuesday">
+              TUE
+            </RowHeader>
+            <span
+              role="cell"
+              tabIndex={0}
+              aria-label="from 9:00 A.M. till 10:00 P.M."
+            >
+              9:00 A.M. - 10:00 P.M.
+            </span>
+          </Row>
+          <Row role="row">
+            <RowHeader role="rowheader" tabIndex={0} aria-label="Wednesday">
+              WED
+            </RowHeader>
+            <span
+              role="cell"
+              tabIndex={0}
+              aria-label="from 9:00 A.M. till 10:00 P.M."
+            >
+              9:00 A.M. - 10:00 P.M.
+            </span>
+          </Row>
+          <Row role="row">
+            <RowHeader role="rowheader" tabIndex={0} aria-label="Thursday">
+              THU
+            </RowHeader>
+            <span
+              role="cell"
+              tabIndex={0}
+              aria-label="from 9:00 A.M. till 10:00 P.M."
+            >
+              9:00 A.M. - 10:00 P.M.
+            </span>
+          </Row>
+          <Row role="row">
+            <RowHeader role="rowheader" tabIndex={0} aria-label="Friday">
+              FRI
+            </RowHeader>
+            <span
+              role="cell"
+              tabIndex={0}
+              aria-label="from 9:00 A.M. till 10:00 P.M."
+            >
+              9:00 A.M. - 10:00 P.M.
+            </span>
+          </Row>
+          <Row role="row">
+            <RowHeader role="rowheader" tabIndex={0} aria-label="Saturday">
+              SAT
+            </RowHeader>
+            <span
+              role="cell"
+              tabIndex={0}
+              aria-label="from 12:00 A.M. till 10:00 P.M."
+            >
+              12:00 A.M. - 10:00 P.M.
+            </span>
+          </Row>
+          <Row role="row">
+            <RowHeader role="rowheader" tabIndex={0} aria-label="Sunday">
+              SUN
+            </RowHeader>
+            <span
+              role="cell"
+              tabIndex={0}
+              aria-label="from 12:00 A.M. till 10:00 P.M."
+            >
+              12:00 A.M. - 10:00 P.M.
+            </span>
+          </Row>
+        </BHContainer>
+      </Wrapper>
     </Content>
   </Container>
 );
--- src/pages/contact/contact.styles.jsx
+++ src/pages/contact/contact.styles.jsx
@@ -13,10 +13,15 @@ export const Container = styled.div`
 
 export const Content = styled.main`
   display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   position: absolute;
   top: 0;
   left: 0;
   padding: 120px;
+  width: 100%;
+  height: 100%;
 `;
 
 export const Info = styled.div`
@@ -26,7 +31,7 @@ export const Info = styled.div`
 
   span {
     font-weight: bold;
-    font-size: calc(25px + 1vw);
+    font-size: calc(15px + 1vw);
   }
 `;
 
@@ -35,6 +40,7 @@ export const BHContainer = styled.div`
   flex-direction: column;
   flex-wrap: wrap;
   text-align: start;
+  font-weight: bold;
 
   & > * {
     flex-grow: 1;
@@ -43,10 +49,33 @@ export const BHContainer = styled.div`
 
 export const Row = styled.div`
   display: flex;
+  margin-bottom: 5px;
+  & :nth-child(2) {
+    width: 85%;
+  }
 `;
 
 export const RowHeader = styled.span`
   display: inline-block;
   flex: 1;
   margin-right: 5px;
+`;
+
+export const ImgContainer = styled.div`
+  width: 500px;
+  height: auto;
+  margin-bottom: 50px;
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 15px;
+    box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.35);
+  }
+`;
+
+export const Wrapper = styled.div`
+  display: inline-flex;
+  & > * {
+    margin-right: 15px;
+  }
 `;
