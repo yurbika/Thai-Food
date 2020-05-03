a7245b2b5753f73c3aa64f844ea2ca5af3d0b49d
Berkay Yurdagül <yurdaguel.b@gmail.com>
2020-05-03 21:44:41 +0200

    adding content on contact

--- src/pages/contact/contact.component.jsx
+++ src/pages/contact/contact.component.jsx
@@ -3,10 +3,66 @@ import React from "react";
 //components
 import Background from "../../components/background/background.component";
 
+//styles
+import { Container, Content, Info } from "./contact.styles";
+
 const Contact = () => (
-  <div>
-    <Background />
-  </div>
+  <Container>
+    <Background className={"background"} />
+    <Content>
+      <Info>
+        <span>Thai Food</span>
+        <span>Cranachstraße 51</span>
+        <span>31691 Niedersachsen</span>
+        <span>+49 157 6874 3357</span>
+      </Info>
+
+      <div role="table" aria-label="Business hours">
+        <div role="row">
+          <span role="rowheader" aria-label="Monday">
+            MON
+          </span>
+          <span role="cell">9:00 A.M. - 10:00 P.M.</span>
+        </div>
+        <div role="row">
+          <span role="rowheader" aria-label="Tuesday">
+            TUE
+          </span>
+          <span role="cell">9:00 A.M. - 10:00 P.M.</span>
+        </div>
+        <div role="row">
+          <span role="rowheader" aria-label="Wednesday">
+            WED
+          </span>
+          <span role="cell">9:00 A.M. - 10:00 P.M.</span>
+        </div>
+        <div role="row">
+          <span role="rowheader" aria-label="Thursday">
+            THU
+          </span>
+          <span role="cell">9:00 A.M. - 10:00 P.M.</span>
+        </div>
+        <div role="row">
+          <span role="rowheader" aria-label="Friday">
+            FRI
+          </span>
+          <span role="cell">9:00 A.M. - 10:00 P.M.</span>
+        </div>
+        <div role="row">
+          <span role="rowheader" aria-label="Saturday">
+            SAT
+          </span>
+          <span role="cell">12:00 A.M. - 10:00 P.M.</span>
+        </div>
+        <div role="row">
+          <span role="rowheader" aria-label="Sunday">
+            SUN
+          </span>
+          <span role="cell">12:00 A.M. - 10:00 P.M.</span>
+        </div>
+      </div>
+    </Content>
+  </Container>
 );
 
 export default Contact;
--- src/pages/contact/contact.styles.jsx
+++ src/pages/contact/contact.styles.jsx
@@ -1,1 +1,31 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+  position: fixed;
+  height: 100vh;
+  overflow: hidden;
+
+  & .background {
+    position: absolute;
+    z-index: -2;
+  }
+`;
+
+export const Content = styled.main`
+  display: flex;
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding: 120px;
+`;
+
+export const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+
+  span {
+    font-weight: bold;
+    font-size: calc(25px + 1vw);
+  }
+`;
