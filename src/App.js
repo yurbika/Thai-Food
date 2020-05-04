9b9a44d1a72453f86d052ea077c55aed3afa379a
Berkay Yurdagül <yurdaguel.b@gmail.com>
2020-05-04 17:16:30 +0200

    adjusting index.html app.js for performance

--- src/App.js
+++ src/App.js
@@ -1,67 +1,49 @@
 import React, { Suspense, lazy } from "react";
-import { createStructuredSelector } from "reselect";
-import { Switch, Route } from "react-router-dom";
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 
 import "./App.css";
 
 //components
-import Home from "./pages/home/home.component";
-import Menu from "./pages/menu/menu.component";
 import Navbar from "./components/navbar/navbar.component";
 import Button from "./components/button/button.component";
 import LoadingScreen from "./components/loadingScreen/loadingScreen.component";
 import Popup from "./components/popup/popup.component";
-import Contact from "./pages/contact/contact.component";
 
 //redux
-import { setIsLoading, setFirstMount } from "./redux/app/app.action";
-import { selectIsLoading } from "./redux/app/app.selectors";
-
 import { selectShowPopup } from "./redux/popup/popup.selectors";
-// const Home = lazy(() => {
-//   return new Promise((resolve) => {
-//     setTimeout(() => resolve(import("./pages/home/home.component")), 1500);
-//   });
-// });
+
+const Home = lazy(() => import("./pages/home/home.component"));
+
+const Menu = lazy(() => import("./pages/menu/menu.component"));
+
+const Contact = lazy(() => import("./pages/contact/contact.component"));
 
 class App extends React.Component {
-  componentWillMount() {
-    this.props.setIsLoading(true);
-  }
-
-  componentDidMount() {
-    this.props.setFirstMount();
-  }
-
   render() {
     return (
-      <div className="App">
-        {/*  <LoadingScreen /> */}
-        <Popup active={this.props.showPopup} />
-        <Suspense fallback={<LoadingScreen />}>
-          <Navbar />
-          <Button menu />
-          <Button left />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/menu" component={Menu} />
-            <Route exact path="/contact" component={Contact} />
-          </Switch>
-        </Suspense>
-      </div>
+      <Router>
+        <div className="App">
+          <Popup active={this.props.showPopup} />
+          <Suspense fallback={<LoadingScreen />}>
+            <Navbar />
+            <Button menu />
+            <Button left />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/menu" component={Menu} />
+              <Route exact path="/contact" component={Contact} />
+            </Switch>
+          </Suspense>
+        </div>
+      </Router>
     );
   }
 }
 
 const mapStateToProps = createStructuredSelector({
-  isLoading: selectIsLoading,
   showPopup: selectShowPopup,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  setIsLoading: (boolean) => dispatch(setIsLoading(boolean)),
-  setFirstMount: () => dispatch(setFirstMount()),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps)(App);
